685239f935e03e1fca635ece4ded9097fc45948e
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-08-26 12:47:26 +0200

    lot of stuff

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleroux <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 11:09:08 by jleroux           #+#    #+#             */
+/*   Updated: 2022/08/26 12:47:12 by jleroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdio.h>
 #include <fcntl.h>
@@ -6,18 +18,14 @@
 
 #define STDIN 0
 #define STDOUT 1
-#define READ 0
-#define WRITE 1
 
-
-static void err_msg_exit(char *msg)
+static void	err_msg_exit(char *msg)
 {
-	perror(msg);
+	write(2, msg, ft_strlen(msg));
 	exit(1);
 }
 
-
-int	path_error(char **paths, int i)
+static int	path_error(char **paths, int i)
 {
 	if (!paths[i])
 	{
@@ -55,51 +63,42 @@ static char	*get_path(char *program_name, char **envp)
 	return (path);
 }
 
-static void exec_prev_cmd(int ncmds, char ***cmds, char **envp, int output[2], int io[2]);
-
 static int	exec_nth_cmd(int ncmds, char ***cmds, char **envp, int io[2])
 {
 	pid_t	pid;
 	int		status;
 	int		input[2];
+	int		output[2];
 
-    if (ncmds < 0)
-		return (1);
-    if (ncmds >= 0) {
-        if (pipe(input) != 0)
-            err_msg_exit("Failed to create pipe");
-        if ((pid = fork()) < 0)
-            err_msg_exit("Failed to fork");
-        if (pid == 0 && ncmds > 1)
-            exec_prev_cmd(ncmds, cmds, envp, input, io);
-		waitpid(pid, &status, 0);
-		if (ncmds == 0)
+	if (ncmds > 0)
+	{
+		if (pipe(input) != 0)
+			err_msg_exit("Failed to create pipe\n");
+		pid = fork();
+		if (pid < 0)
+			err_msg_exit("Failed to fork\n");
+		if (pid == 0 && ncmds > 0)
 		{
-			fprintf(stderr, "io[0] = %d\n", io[0]);
-			dup2(input[0], io[0]);
-			fprintf(stderr, "test\n");
+			dup2(output[1], STDOUT);
+			close(output[0]);
+			close(output[1]);
+			exec_nth_cmd(ncmds - 1, cmds, envp, io);
 		}
-		else
-			dup2(input[0], 0);
-        close(input[0]);
-        close(input[1]);
-    }
-    if (execve(get_path(cmds[ncmds][0], envp), cmds[ncmds], envp) == -1)
-	    err_msg_exit("Failed on exec");
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
+		waitpid(pid, &status, 0);
+		dup2(input[0], STDIN);
+		close(input[0]);
+		close(input[1]);
+	}
+	if (ncmds == 0)
+		dup2(io[0], 0);
+	fprintf(stderr, "%s\n", get_path(cmds[ncmds][0], envp));
+	if (execve(get_path(cmds[ncmds][0], envp),
+		cmds[ncmds], envp) == -1)
+		err_msg_exit("Failed on exec\n");
 	return (1);
 }
 
-static void exec_prev_cmd(int ncmds, char ***cmds, char **envp, int output[2], int io[2]) {
-    dup2(output[1], 1);
-    close(output[0]);
-    close(output[1]);
-	fprintf(stderr, "test\n");
-    exec_nth_cmd(ncmds - 1, cmds, envp, io);
-}
-
-static int check_io(int io[2], char *infile, char *outfile)
+static int	check_io(int io[2], char *infile, char *outfile)
 {
 	io[0] = open(infile, O_RDONLY);
 	io[1] = open(outfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
@@ -110,19 +109,32 @@ static int check_io(int io[2], char *infile, char *outfile)
 
 int	main(int ac, char **av, char **envp)
 {
-	char	**cmds[ac - 3];
-	int		ncmds;
+	char	***cmds;
+	pid_t	pid;
+	int		status;
 	int		io[2];
 	int		i;
 
 	if (ac < 5)
-		err_msg_exit("Usage: ./pipex infile \"cmd1 args[]\" \"cmd2 args[]\" outfile");
-	if (check_io(io, av[1], av[ac - 1]))
+		err_msg_exit(
+			"Usage: ./pipex infile \"cmd1 args[]\" \"cmd2 args[]\" outfile\n");
+	if (check_io(io, av[1], av[ac - 1]) == -1)
 		return (1);
-	ncmds = ac - 4;
-	//malloc cmds;
+	//close(io[0]);
+	cmds = malloc((ac - 3) * sizeof(char ***));
 	i = -1;
-	while (++i < ncmds)
+	while (++i < ac - 3)
 		cmds[i] = ft_split(av[i + 2], ' ');
-	return (exec_nth_cmd(ncmds, cmds, envp, io));
+	pid = fork();
+	if (dup2(io[1], STDOUT) == -1)
+		return (1);
+	if (pid == 0)
+		return (exec_nth_cmd(ac - 4, cmds, envp, io));
+	waitpid(pid, &status, 0);
+	i = -1;
+	while (++i < ac - 3)
+		ft_free_tab(cmds[i]);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (0);
 }
